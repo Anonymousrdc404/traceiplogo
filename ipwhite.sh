a8d74215155ffb15fd6a1c06e1655f9b174efcdb
White404 <whitetech404@gmail.com>
2025-05-09 11:59:29 +0200

    Add files via upload
    MIT License (H4CK3R Version)
    
    Copyright (c) 2025 WHITE404 - ANONYMOUS404
    
    Permission is hereby granted, free of charge, to any person obtaining a copy
    of this software and associated documentation files (the "Software"), to use,
    copy, modify, merge, publish, and/or distribute the Software for educational
    or ethical hacking purposes only, subject to the following conditions:
    
    1. The above copyright notice and this permission notice shall be included in
       all copies or substantial portions of the Software.
    
    2. The name "WHITE404" and group "ANONYMOUS404" must be credited in any public
       forks or re-uploads.
    
    3. The Software **shall not** be used for:
       - Blackhat, malicious, or illegal activity
       - Surveillance or harm to others
       - Any government or corporate espionage (unless it's consensual 😈)
    
    4. The author assumes **no liability** for any damage, legal issue, or karma 
       caused by the use or misuse of this software.
    
    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
    IMPLIED. USE AT YOUR OWN RISK.
    
    Stay stealthy. Stay free. 🖤

--- ipwhite.sh
+++ ipwhite.sh
@@ -1,58 +1,81 @@
 #!/bin/bash
 
-# Nom du Dev: WHITE404 | Groupe: Anonymous404
+# Dev: WHITE404 | Anonymous404
 
 while true; do
     clear
 
-    echo -e "\e[1;35m"
-    echo "████████╗██████╗  █████╗  ██████╗███████╗     ██╗      ██████╗  ██████╗  ██████╗ "
-    echo "╚══██╔══╝██╔══██╗██╔══██╗██╔════╝██╔════╝     ██║     ██╔═══██╗██╔════╝ ██╔═══██╗"
-    echo "   ██║   ██████╔╝███████║██║     █████╗       ██║     ██║   ██║██║  ███╗██║   ██║"
-    echo "   ██║   ██╔═══╝ ██╔══██║██║     ██╔══╝       ██║     ██║   ██║██║   ██║██║   ██║"
-    echo "   ██║   ██║     ██║  ██║╚██████╗███████╗     ███████╗╚██████╔╝╚██████╔╝╚██████╔╝"
-    echo "   ╚═╝   ╚═╝     ╚═╝  ╚═╝ ╚═════╝╚══════╝     ╚══════╝ ╚═════╝  ╚═════╝  ╚═════╝ "
-    echo -e "\e[0m"
+    echo -e "\e[1;95m"
+    cat << "EOF"
+████████╗██████╗  █████╗  ██████╗███████╗     ██╗      ██████╗  ██████╗  ██████╗ 
+╚══██╔══╝██╔══██╗██╔══██╗██╔════╝██╔════╝     ██║     ██╔═══██╗██╔════╝ ██╔═══██╗
+   ██║   ██████╔╝███████║██║     █████╗       ██║     ██║   ██║██║  ███╗██║   ██║
+   ██║   ██╔═══╝ ██╔══██║██║     ██╔══╝       ██║     ██║   ██║██║   ██║██║   ██║
+   ██║   ██║     ██║  ██║╚██████╗███████╗     ███████╗╚██████╔╝╚██████╔╝╚██████╔╝
+   ╚═╝   ╚═╝     ╚═╝  ╚═╝ ╚═════╝╚══════╝     ╚══════╝ ╚═════╝  ╚═════╝  ╚═════╝ 
+EOF
+    echo -e "\e[0m\e[1;36m               [✦] TOOL DE RESEAU | CODED BY WHITE404 ✦\e[0m"
+    echo -e "\e[1;97m═════════════════════════════════════════════════════════════════════\e[0m"
+    echo -e "\e[1;32m[1] Voir mon IP publique"
+    echo -e "[2] Localiser une IP"
+    echo -e "[3] Interfaces et IP locales"
+    echo -e "[4] WHOIS sur IP ou domaine"
+    echo -e "[5] PING une cible"
+    echo -e "[6] Traceroute"
+    echo -e "[7] Scan de ports rapide (nmap)"
+    echo -e "[8] Quitter\e[0m"
+    echo -e "\e[1;97m═════════════════════════════════════════════════════════════════════\e[0m"
     
-    echo -e "\e[1;36m        _________"
-    echo -e "       / ======= \\"
-    echo -e "      / __________\\     IP TRACKER - WHITE404"
-    echo -e "     | ___________ |    Groupe: Anonymous404"
-    echo -e "     | | -       | |"
-    echo -e "     | |         | |    Menu:"
-    echo -e "     | |_________| |      1. 🌍 Voir mon IP publique"
-    echo -e "     \\=___________/       2. 🛰️  Géolocaliser une IP"
-    echo -e "     / ''''''''''''\\      3. 🔧 Infos IP locales (interfaces)"
-    echo -e "    / ::::::::::::: \\     4. ❌ Quitter"
-    echo -e "   (_________________)\e[0m"
-
     read -p $'\e[1;33mCHOIX >>> \e[0m' CHOIX
 
     case $CHOIX in
         1)
-            echo -e "\e[1;32m[+] Votre IP publique est :\e[0m"
+            echo -e "\n\e[1;36m[+] IP publique :\e[0m"
             curl -s https://ipinfo.io/ip
-            read -p $'\nAppuyez sur Entrée pour revenir au menu...'
+            read -p $'\nAppuyez sur Entrée pour continuer...'
             ;;
         2)
             read -p $'\e[1;34m[?] IP à localiser : \e[0m' IP
-            echo -e "\n\e[1;36m[+] Infos pour $IP :\e[0m"
+            echo -e "\n\e[1;36m[+] Géolocalisation via ipinfo.io :\e[0m"
             curl -s ipinfo.io/$IP | jq .
-            read -p $'\nAppuyez sur Entrée pour revenir au menu...'
+            read -p $'\nAppuyez sur Entrée...'
             ;;
         3)
-            echo -e "\e[1;34m[+] Informations IP et interfaces réseau :\e[0m"
+            echo -e "\n\e[1;36m[+] Interfaces réseau :\e[0m"
             ip a
-            echo -e "\n\e[1;34m[+] Routes :\e[0m"
+            echo -e "\n\e[1;36m[+] Routes :\e[0m"
             ip route
-            read -p $'\nAppuyez sur Entrée pour revenir au menu...'
+            read -p $'\nEntrée pour retour...'
             ;;
         4)
-            echo -e "\e[1;31m[!] Sortie du script. Merci WHITE404 ✌️\e[0m"
+            read -p $'\e[1;34m[?] Domaine ou IP : \e[0m' HOST
+            echo -e "\n\e[1;36m[+] WHOIS info :\e[0m"
+            whois $HOST | head -n 30
+            read -p $'\nEntrée pour retour...'
+            ;;
+        5)
+            read -p $'\e[1;34m[?] Hôte à ping : \e[0m' PING
+            echo -e "\n\e[1;36m[+] Résultat :\e[0m"
+            ping -c 4 $PING
+            read -p $'\nEntrée pour retour...'
+            ;;
+        6)
+            read -p $'\e[1;34m[?] Hôte pour traceroute : \e[0m' TRG
+            traceroute $TRG
+            read -p $'\nEntrée pour retour...'
+            ;;
+        7)
+            read -p $'\e[1;34m[?] IP ou domaine à scanner : \e[0m' TGT
+            echo -e "\n\e[1;36m[+] Scan des 1000 ports communs...\e[0m"
+            nmap -T4 $TGT
+            read -p $'\nEntrée pour retour...'
+            ;;
+        8)
+            echo -e "\e[1;31m[!] Merci d’avoir utilisé TRACE LOC. À bientôt, h4ck3r 👻\e[0m"
             exit 0
             ;;
         *)
-            echo -e "\e[1;31m[X] Choix invalide. Essaie encore.\e[0m"
+            echo -e "\e[1;31m[X] Choix invalide.\e[0m"
             sleep 2
             ;;
     esac
